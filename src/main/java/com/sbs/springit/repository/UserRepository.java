ed334fc4734d1cb32d46f30231210544248f6079
Sivertsen <AB55050@dnbnor.net>
2019-02-20 22:14:45 +0100

    Adding user roles, userDetailService and userRepository

--- src/main/java/com/sbs/springit/repository/UserRepository.java
+++ src/main/java/com/sbs/springit/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.sbs.springit.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sbs.springit.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	Optional<User> findByEmail(String email);
 
 }
---
+++ src/main/java/com/sbs/springit/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.sbs.springit.security;
+
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sbs.springit.domain.User;
+import com.sbs.springit.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl {
+	
+	private UserRepository userRepository;
+	
+	public UserDetailsServiceImpl(UserRepository userRepository) {			
+		this.userRepository = userRepository;			
+	}
+	
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+		Optional<User> user = userRepository.findByEmail(email);
+		if( !user.isPresent() ) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return user.get();
+	}
+
+}
